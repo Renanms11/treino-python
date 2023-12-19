a3d23c1cd00e6bb3873e86117a28c897f83a5733
RenanSena2023 <renansena20152proton.me>
2023-12-19 20:59:49 -0300

    fibonacci em algoritmo

--- Treinofunçõesrecursivas.py
+++ Treinofunçõesrecursivas.py
@@ -1,33 +1,16 @@
-
-def ordenarListaNumero(listas):
-    auxiliar = 0
-    tamanhoLista= len(listas)
-    for i in range(tamanhoLista -1):
-        for ii in range(tamanhoLista-1):
-            if listas[ii] > listas[ii+1]:
-                auxiliar = listas[ii+1]
-                listas[ii+1] = listas[ii]
-                listas[ii] = auxiliar
-
-def inverterString(texto):
-    arrayChar = len(texto)
-    listaTexto = list(texto)
-
-    for i in range(arrayChar // 2):
-        aux = listaTexto[i]
-        listaTexto[i] = listaTexto[arrayChar - 1 - i]
-        listaTexto[arrayChar - 1 -i] = aux
-        texto = ''.join(listaTexto)
-
-    return texto
+def fibonacciAlgoritmo(x):
+    a = 0
+    b = 1
+    for i in range(x):
+        print(a , end = " ")
+        aux = b
+        b += a
+        a =aux
 
 
 
 
-texto = input("digite seu nome completo :")
-texto =inverterString(texto)
-print(texto)
 
-
+fibonacciAlgoritmo(10)
 
 
