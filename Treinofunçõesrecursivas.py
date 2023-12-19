7903eccf5f8fd3a53d8a05bded80253cdefe7880
RenanSena2023 <renansena20152proton.me>
2023-12-19 20:48:26 -0300

    algoritmo de inverter uma string

--- Treinofunçõesrecursivas.py
+++ Treinofunçõesrecursivas.py
@@ -1,23 +1,33 @@
 
 def ordenarListaNumero(listas):
     auxiliar = 0
-    tamanhoLista= len(lista)
+    tamanhoLista= len(listas)
     for i in range(tamanhoLista -1):
         for ii in range(tamanhoLista-1):
-            if lista[ii] > lista[ii+1]:
-                auxiliar = lista[ii+1]
-                lista[ii+1] = lista[ii]
-                lista[ii] = auxiliar
+            if listas[ii] > listas[ii+1]:
+                auxiliar = listas[ii+1]
+                listas[ii+1] = listas[ii]
+                listas[ii] = auxiliar
+
+def inverterString(texto):
+    arrayChar = len(texto)
+    listaTexto = list(texto)
+
+    for i in range(arrayChar // 2):
+        aux = listaTexto[i]
+        listaTexto[i] = listaTexto[arrayChar - 1 - i]
+        listaTexto[arrayChar - 1 -i] = aux
+        texto = ''.join(listaTexto)
+
+    return texto
 
 
 
 
-lista =[10,5,8,4,6,1,3,11,14,0,2]
+texto = input("digite seu nome completo :")
+texto =inverterString(texto)
+print(texto)
 
 
-
-ordenarListaNumero(lista)
-
-print(lista)
 
 
