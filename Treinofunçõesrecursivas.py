ea41f68565e4e68e93d96819ff437c6489f83dfb
RenanSena2023 <renansena20152proton.me>
2023-12-21 18:19:14 -0300

    algoritmo de selectioneSort

--- Treinofunçõesrecursivas.py
+++ Treinofunçõesrecursivas.py
@@ -1,8 +1,27 @@
+def selectiveSort(lista):
+
+    posicao =0
+    for  i in range(len(lista )):
+        menor =i
+
+        for j in range(i+1, len(lista)):
+            if lista[j]< lista[menor]:
+                menor = j
+
+        # Se o elemento atual não é o menor, troca-os
+        if lista[i] != lista[menor]:
+            aux = lista[i]
+            lista[i] = lista[menor]
+            lista[menor] = aux
 
 
-def fibonnaci(x, a= 0 , b = 1):
-  if x > 0 :
-      print(a, end=" ")
-      fibonnaci(x-1,b,a+b)
 
-fibonnaci(10)
+lista = [10,5,9,1,3,4,6,2,7,5,8]
+selectiveSort(lista)
+
+for x in lista:
+    print(x , end = " ")
+
+
+
+
