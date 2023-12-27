1d8dc3a76e9da8233f8b95c6591db48b23e62d0a
RenanSena2023 <renansena20152proton.me>
2023-12-27 19:32:45 -0300

    criando atributos de classe e metodos de classe

--- Agencia.py
+++ Agencia.py
@@ -1,6 +1,7 @@
 from Conta import Conta
 class Agencia:
 
+    _Banco = 'Banco novo'
     def __init__(self,numero,gerente ,contas):
         self.numero=numero
         self.gerente = gerente
@@ -9,15 +10,15 @@ class Agencia:
 
 
 
-
-
-
+    @classmethod
+    def get_banco(cls):
+        return cls._Banco
 
 
     def OrdenarContas(self):
         for i in range(len(self.contas)-1):
             index =i
-            for j in range(len(i+1,self.contas)):
+            for j in range(len(i+1,len(self.contas))):
                 if self.contas[j]< self.contas[index]:
                     index = j
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Conta import Conta
 from Cliente import Cliente
 from Extrato import Extrato
+from Agencia import Agencia
 
 cliente1 = Cliente('01382074450','renan miranda sena','rua alameda das mansoes')
 cliente2 = Cliente('06125661377','katianny maia','rua alamenda das mansoes')
@@ -8,6 +9,7 @@ cliente2 = Cliente('06125661377','katianny maia','rua alamenda das mansoes')
 
 
 novaconta = Conta([cliente1,cliente2], '0138202074450',1, 2000)
+Agencia = Agencia(16683,'Renan mraidanas', novaconta)
 
 novaconta.depositar(1500)
 novaconta.retirada(999)
