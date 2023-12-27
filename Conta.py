49d836e58f9d499a97c120e0ad95b01a9cec41cb
RenanSena2023 <renansena20152proton.me>
2023-12-27 16:28:20 -0300

    criando classe extrado e realizando a composição com a classe conta

--- Conta.py
+++ Conta.py
@@ -1,18 +1,25 @@
+import datetime
+
 from Cliente import Cliente
+from Extrato import  Extrato
 
 class Conta:
-    def __init__(self,clientes, cpf, titular,saldo):
+    def __init__(self,clientes, cpf, numero,saldo):
         self.clientes= clientes
         self.cpf=cpf
-        self.titular= titular
+        self.numero= numero
         self.saldo =saldo
+        self.dataAbertura= datetime.datetime.today()
+        self.extrato = Extrato()
 
     def  depositar (self,valor):
         self.saldo += valor
+        self.extrato.transacoes.append(["Deposito",valor,'Data',datetime.datetime.today()])
 
     def  retirada(self,valor):
         if self.saldo > 0 and valor <= self.saldo:
             self.saldo -=valor
+            self.extrato.transacoes.append(['Retirada',valor,'Data',datetime.datetime.today()])
             print('Retirada do valor de {}'.format(valor))
             #print(f'Retirada do Valor de {valor}')
             #print('Retirada do  valor de ' +  str(valor))
@@ -24,6 +31,8 @@ class Conta:
         if self.saldo >0 and self.saldo >= valor:
             self.saldo -=valor
             contaDestino.depositar(valor)
+            self.extrato.transacoes.append(["Transferência",valor,'DAta',datetime.datetime.today()])
 
         else:
             return 'saldo insuficiente !'
+
